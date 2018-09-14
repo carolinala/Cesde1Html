e153b3740d2fce221a1afb349b54ae4a7aa9fd18
Unknown <ibarguencarolina035@gmail.com>
2018-09-14 14:34:03 -0500

    echo :)
    guapura

--- juanjose2/index.php
+++ juanjose2/index.php
@@ -7,11 +7,50 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Proyecto</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>
+             <?php
+             // Esto Es Un Comentario
+                //echo 'Hola Juan, Tia Te Ama  ';
+                //echo '<br>';
+                $SegundaVariable = 1;
+                $PrimeraVariable= 2;
+                $lenguaje = 'html';
+                
+                //echo $PrimeraVariable.''."<br>".$SegundaVariable."<br>";
+                echo $PrimeraVariable - $SegundaVariable;
+                //echo $SegundaVariable;
+                
+                if ($PrimeraVariable ==2 && $SegundaVariable==1){
+                    echo 'Soy Menor';
+                }
+                else{
+                    echo 'Soy Mayor';
+                }
+                echo "<br>";
+                switch ($lenguaje){
+                    case 'php':
+                        echo'php';
+                        break;
+                    case 'javascript':
+                        echo'javascript';
+                        break;
+                    case 'html':
+                        echo'html';
+                        break;
+                }
+                        
+             ?>
+        </h1>
+        <p>
+            <h2>
+                <?php
+                   echo "Caro Carito Caro"
+                ?>
+            </h2>
+        </p>
+     
     </body>
 </html>
