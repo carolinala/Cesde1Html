309eddfcaba100328c09ff030924ca11ca1a3545
Carolina <ibarguencarolina035@gmail.com>
2018-09-26 13:47:15 -0500

    Pagina Web 26092018

---
+++ 26092018/WebPage.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+    
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Web Bootstrap</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/estilos index.css">
+    </head>
+    <body>
+        
+        <header>
+            <!-- Image and text -->
+            <div class="text-center">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">
+                      <img src="img/logo1.jpg" width="30" height="30" class="" alt="">
+                    Amapola
+                  </a>
+                </nav>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light" >    
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link active" href="#">Quienes Somos</a>
+                        <a class="nav-item nav-link active" href="#">Contactános</a>
+                        <a class="nav-item nav-link active" href="#">Inicias Sesión</a>
+                        <a class="nav-item nav-link active" href="#">Registrarse</a>
+                        
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        //
+        <section>
+            
+        </section>
+        //
+        <footer>
+            <div class="container-fluid">
+                <h5>&copy Derechos de Autor</h5>
+            </div>  
+        </footer>
+        <?php
+        // put your code here
+        ?>
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/popper.js"></script>
+    </body>
+</html>
